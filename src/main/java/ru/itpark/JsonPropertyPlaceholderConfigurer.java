d1b68f4e46eed2932489b031eab1625e46896819
almaz.nurmukhametov@rtlabs.ru <almaz.nurmukhametov@rtlabs.ru>
2019-06-19 20:45:18 +0300

    Revert "changed the decision"
    This reverts commit e2429933

--- src/main/java/ru/itpark/JsonPropertyPlaceholderConfigurer.java
+++ src/main/java/ru/itpark/JsonPropertyPlaceholderConfigurer.java
@@ -1,12 +1,11 @@
 package ru.itpark;
 
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.util.CollectionUtils;
+import ru.itpark.models.PostModel;
+import ru.itpark.utils.Parser;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Properties;
 
 
@@ -15,50 +14,10 @@ import java.util.Properties;
  */
 public class JsonPropertyPlaceholderConfigurer extends PropertyPlaceholderConfigurer {
 
-    //    public JsonPropertyPlaceholderConfigurer() throws FileNotFoundException {
-//        setLocation(new ClassPathResource("properties.json"));
-//
-//        Properties properties = new Properties();
-//        PostModel postModel = Parser.getPostModelByFileReader(new FileReader("./src/main/resources/properties.json"));
-//        properties.setProperty("id", postModel.getId().toString());
-//        setProperties(properties);
-//    }
-    public JsonPropertyPlaceholderConfigurer() {
-        setLocation(new ClassPathResource("example.json"));
-    }
-
-/* TODO: В свое оправдание скажу, что пробовал по разному. Но это решение как и предыдущее не нравится!
-    Скорее всего нужно копать в сторону #loadProperties */
-
-    protected Properties mergeProperties() throws IOException {
-        Properties result = new Properties();
-
-        if (this.localOverride) {
-            // Load properties from file upfront, to let local properties override.
-            loadProperties(result);
-        }
-
-        if (this.localProperties != null) {
-            for (Properties localProp : this.localProperties) {
-                CollectionUtils.mergePropertiesIntoMap(localProp, result);
-            }
-        }
-
-        if (!this.localOverride) {
-            // Load properties from file afterwards, to let those properties override.
-            loadProperties(result);
-        }
-
-        Map<String, String> map = new HashMap<>();
-        for (final String name : result.stringPropertyNames())
-            map.put(name.replaceAll("\"", ""), result.getProperty(name).replaceAll("\"", ""));
-
+    public JsonPropertyPlaceholderConfigurer() throws FileNotFoundException {
         Properties properties = new Properties();
-        for (String value : map.keySet()) {
-            properties.setProperty(value, map.get(value));
-        }
-
-        return properties;
+        PostModel postModel = Parser.getPostModelByFileReader(new FileReader("./src/main/resources/properties.json"));
+        properties.setProperty("id", postModel.getId().toString());
+        setProperties(properties);
     }
-
 }
